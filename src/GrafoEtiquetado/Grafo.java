fe7d174c942a2435c651fda5addf8231df523900
Jdxa <dossantosjoaquin1@gmail.com>
2026-07-07 10:19:23 -0300

    nodos de grafos implementados

--- src/GrafoEtiquetado/Grafo.java
+++ src/GrafoEtiquetado/Grafo.java
@@ -1,5 +1,7 @@
 package GrafoEtiquetado;
 public class Grafo {
+    
+    // atributos
     private NodoVert inicio = null;
     public Grafo(){
 
--- src/GrafoEtiquetado/NodoAdy.java
+++ src/GrafoEtiquetado/NodoAdy.java
@@ -2,6 +2,28 @@ package GrafoEtiquetado;
 
 public class NodoAdy {
     
+    // atributos
     private NodoVert vertice;
     private NodoAdy sigAdyacente;
+
+    public NodoAdy(NodoVert vert, NodoAdy ady){
+        this.vertice = vert;
+        this.sigAdyacente = ady;
+    }
+
+    public NodoVert getVertice(){
+        return this.vertice;
+    }
+
+    public void setVertice(NodoVert vert){
+        this.vertice = vert;
+    }
+
+    public NodoAdy getSigAdyacente(){
+        return this.sigAdyacente;
+    }
+
+    public void setSigAdyacente(NodoAdy ady){
+        this.sigAdyacente = ady;
+    }
 }
