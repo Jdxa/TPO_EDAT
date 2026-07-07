ca25b41d1874346b88b10b084af0d1f1183f4f44
Jdxa <dossantosjoaquin1@gmail.com>
2026-07-07 10:34:55 -0300

    cambios a NodoAdy y metodo en grafo

--- src/GrafoEtiquetado/Grafo.java
+++ src/GrafoEtiquetado/Grafo.java
@@ -6,5 +6,27 @@ public class Grafo {
     public Grafo(){
 
     }        
+    
+
+    private NodoVert ubicarVertice(Object buscado){
+        //busca que buscado no exista, si existe no se puede insertar
+        NodoVert aux = this.inicio;
+        while (aux != null && !aux.getElem().equals(buscado)) {
+            aux = aux.getSigVertice();
+        }
+        return aux;
+    }
+
+    public boolean insertarVertice(Object nuevoVertice){
+        //no hay vertices repetidos
+        boolean exito = false;
+        NodoVert aux = this.ubicarVertice(nuevoVertice);
+        if (aux == null) {
+            // si no esta repetido lo inserta
+            this.inicio = new NodoVert(nuevoVertice, aux, null); // no conozco como se unen asique null
+        }
+
+        return exito;
+    }
 
 }
--- src/GrafoEtiquetado/NodoAdy.java
+++ src/GrafoEtiquetado/NodoAdy.java
@@ -5,6 +5,7 @@ public class NodoAdy {
     // atributos
     private NodoVert vertice;
     private NodoAdy sigAdyacente;
+    private Object etiqueta;
 
     public NodoAdy(NodoVert vert, NodoAdy ady){
         this.vertice = vert;
@@ -25,5 +26,13 @@ public class NodoAdy {
 
     public void setSigAdyacente(NodoAdy ady){
         this.sigAdyacente = ady;
+    }
+
+    public Object getEtiqueta(){
+        return this.etiqueta;
+    }
+
+    public void setEtiqueta(Object nuevaEtiqueta){
+        this.etiqueta = nuevaEtiqueta;
     }
 }
