a14c8ee02bd8192425a3a1c2c356b93842ffaca3
fixy <Lucasperoni2005@gmail.com>
2026-07-30 18:12:20 -0300

    alfa

--- src/Modelo/Desafio.java
+++ src/Modelo/Desafio.java
@@ -1,5 +1,9 @@
 package Modelo;
 
 public class Desafio {
+    private int puntajeOtorga;
+    private int codHabitacion;
+    private String nombre;
+    private String tipo;
     
 }
--- src/Modelo/Equipo.java
+++ src/Modelo/Equipo.java
@@ -1,5 +1,10 @@
 package Modelo;
 
 public class Equipo {
+    private String nombre;
+    private int puntajeExigido;
+    private int puntajeAcumulado;
+    private Habitacion habitacionActual;
+    private int puntajeActual;
     
 }
