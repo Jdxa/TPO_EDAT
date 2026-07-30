5be3d09b1ef4e2ef96c0e568f5eb2af9d2364998
Jdxa <dossantosjoaquin1@gmail.com>
2026-07-30 19:06:43 -0300

    equipo y desafio

--- src/Modelo/Desafio.java
+++ src/Modelo/Desafio.java
@@ -6,4 +6,42 @@ public class Desafio {
     private String nombre;
     private String tipo;
     
+    public Desafio(int unPuntaje, int unCodigoHab, String unNombre, String unTipo){
+        this.puntajeOtorga = unPuntaje;
+        this.codHabitacion = unCodigoHab;
+        this.nombre = unNombre;
+        this.tipo = unTipo;
+    }
+
+    public int getPuntaje(){
+        return this.puntajeOtorga;
+    }
+
+    public void setPuntaje(int unPuntaje){
+        this.puntajeOtorga = unPuntaje;
+    }
+    
+    public int getCodigoHabitacion(){
+        return this.codHabitacion;
+    }
+
+    public void setCodigoHabitacion(int unCodigo){
+        this.codHabitacion = unCodigo;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setNombre(String unNombre){
+        this.nombre = unNombre;
+    }
+    
+    public String getTipo(){
+        return this.tipo;
+    }
+
+    public void setTipo(String unTipo){
+        this.tipo = unTipo;
+    }
 }
